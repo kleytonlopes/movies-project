bb627d1088b8e2a9dfa986d7d5673f80925e19c8
Kleyton Lopes <kleyton.lop@gmail.com>
2021-10-03 14:32:10 -0300

    feat: implement Movie entity

--- MoviesProject/UseCases/Entities/Movie.swift
+++ MoviesProject/UseCases/Entities/Movie.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 struct Movie {
-    
+    let id: Int
+    let title: String
+    let overview: String
+    let poster: String?
+    let voteAverage: Float
+    let releaseDate: String?
+}
+
+extension Movie: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case overview
+        case poster = "poster_path"
+        case voteAverage = "vote_average"
+        case releaseDate = "release_date"
+    }
 }
