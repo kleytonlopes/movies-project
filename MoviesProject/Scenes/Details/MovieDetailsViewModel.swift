1765e77ddbf4961ab72be9559c470df01a222a57
Kleyton Lopes <kleyton.lop@gmail.com>
2021-10-03 16:37:52 -0300

    feat: male MovieDetailsViewModel implementation

--- MoviesProject/Scenes/Details/MovieDetailsViewModel.swift
+++ MoviesProject/Scenes/Details/MovieDetailsViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import Combine
+
+class MovieDetailsViewModel: MovieDetailsViewModelProtocol {
+    private let movieId: Int
+    private let useCase: MoviesUseCaseProtocol
+    
+    init(movieId: Int, useCase: MoviesUseCaseProtocol) {
+        self.movieId = movieId
+        self.useCase = useCase
+    }
+    
+    func transform(input: MovieDetailsViewModelInput) -> MovieDetailsViewModelOutput {
+        let movieDetails = input.appear
+            .flatMap({[unowned self] _ in self.useCase.movieDetails(with: self.movieId) })
+            .map({ result -> MovieDetailsState in
+                switch result {
+                    case .success(let movie): return .success(self.toViewModel(from: movie))
+                    case .failure(let error): return .failure(error)
+                }
+            })
+            .eraseToAnyPublisher()
+        let loading: MovieDetailsViewModelOutput = input.appear.map({_ in .loading }).eraseToAnyPublisher()
+
+        //juntando os dois
+        return Publishers.Merge(loading, movieDetails).removeDuplicates().eraseToAnyPublisher()
+    }
+    
+    private func toViewModel(from movie: Movie) -> MovieViewModel {
+        return MovieViewModelBuilder.viewModel(from: movie)
+    }
+}
