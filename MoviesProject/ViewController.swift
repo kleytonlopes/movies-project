85804908bcdc6af426799abbfe88e385c0d998e9
Kleyton Lopes <kleyton.lop@gmail.com>
2021-10-03 12:26:45 -0300

    config: initial commit with template project

---
+++ MoviesProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesProject
+//
+//  Created by Kleyton Lopes on 03/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
